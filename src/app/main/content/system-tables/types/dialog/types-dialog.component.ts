84cde89732852b38ce667cc706fb3a607905e0cb
esteban <estebanbacl@outlook.com>
2018-06-17 23:07:31 -0500

    bug guardar typos de riesgos

--- src/app/main/content/system-tables/types/dialog/types-dialog.component.ts
+++ src/app/main/content/system-tables/types/dialog/types-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { TypesRiskModel } from '../model/types-risk.model';
-import {FormType} from '../../../commons/form-type.enum';
+import { TypesRiskService } from '../../../system-tables/types/service/types-risk.service';
 
 
 @Component({
@@ -13,12 +13,14 @@ import {FormType} from '../../../commons/form-type.enum';
 })
 export class TypesDialogComponent implements OnInit {
   showExtraToFields = false;
+  restData: any;
   composeForm: FormGroup;
   typesRiskModel = new TypesRiskModel();
 
   constructor(
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<TypesDialogComponent>,
+    private typesRiskService: TypesRiskService,
     @Inject(MAT_DIALOG_DATA) private data: any
   ) {
   }
@@ -35,7 +37,15 @@ export class TypesDialogComponent implements OnInit {
 
   saveRisk() {
     const tipeRisk = <TypesRiskModel> this.composeForm.getRawValue();
+    this.saveTypeRisk(tipeRisk)
     this.dialogRef.close(tipeRisk);
+  }
+
+  saveTypeRisk(riskTypes) {
+    this.typesRiskService.postTypeRisk(riskTypes).subscribe((data: any) => {
+      this.restData = data;
+      console.log(this.restData);
+    });
   }
 
   private dataForm() {
--- src/app/main/content/system-tables/types/types.component.ts
+++ src/app/main/content/system-tables/types/types.component.ts
@@ -1,49 +1,42 @@
-import { Component, OnInit} from '@angular/core';
-import { TypesDialogComponent } from '../types/dialog/types-dialog.component';
-import { MatDialog } from '@angular/material';
-import { TypesRiskService } from '../../system-tables/types/service/types-risk.service';
+import {Component, OnInit} from '@angular/core';
+import {TypesDialogComponent} from '../types/dialog/types-dialog.component';
+import {MatDialog} from '@angular/material';
+import {TypesRiskService} from '../../system-tables/types/service/types-risk.service';
 
 @Component({
-    selector: 'risk-types',
-    templateUrl: './types.component.html',
-    styleUrls: ['./types.component.scss']
+  selector: 'risk-types',
+  templateUrl: './types.component.html',
+  styleUrls: ['./types.component.scss']
 })
 export class SystemTypesComponent implements OnInit {
-    riskTypes: any[];
-    restData: any;
-    dialogRef: any;
-    loadingIndicator = true;
-    reorderable = true;
+  riskTypes: any[];
+  restData: any;
+  dialogRef: any;
+  loadingIndicator = true;
+  reorderable = true;
 
-    constructor(private typesRiskService: TypesRiskService, public dialog: MatDialog) {
+  constructor(private typesRiskService: TypesRiskService, public dialog: MatDialog) {
 
-    }
+  }
 
-    ngOnInit(){
-      this.riskTypes = [];
-        this.typesRiskService.getTypeRisk().subscribe((data: any) => {
-          this.riskTypes = data;
-          this.loadingIndicator = false;
-          });
-    }
+  ngOnInit() {
+    this.riskTypes = [];
+    this.typesRiskService.getTypeRisk().subscribe((data: any) => {
+      this.riskTypes = data;
+      this.loadingIndicator = false;
+    });
+  }
 
-    saveTypeRisk(riskTypes){
-      this.typesRiskService.postTypeRisk(riskTypes[0]).subscribe((data: any) => {
-        this.restData = data;
-        console.log(this.restData);
+
+  typesDialog() {
+    this.dialogRef = this.dialog.open(TypesDialogComponent, {
+      panelClass: 'types-dialog'
+    });
+    this.dialogRef.afterClosed()
+      .subscribe(response => {
+        this.riskTypes.push(response);
+        this.riskTypes = [...this.riskTypes];
+        this.loadingIndicator = false;
       });
-    }
-
-    typesDialog(){
-      this.dialogRef = this.dialog.open(TypesDialogComponent, {
-        panelClass: 'types-dialog'
-      });
-      this.dialogRef.afterClosed()
-        .subscribe(response => {
-          this.riskTypes.push(response);
-          this.saveTypeRisk(this.riskTypes);
-          this.riskTypes = [...this.riskTypes];
-          this.loadingIndicator = false;
-        });
-    }
+  }
 }
