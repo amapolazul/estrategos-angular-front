e4dc3aa8957c511a0c9754eefe20ff675e53c692
esteban <estebanbacl@outlook.com>
2018-06-17 21:19:04 -0500

    guardar tipos de riesgos

--- src/app/main/content/system-tables/types/dialog/types-dialog.component.ts
+++ src/app/main/content/system-tables/types/dialog/types-dialog.component.ts
@@ -1,27 +1,52 @@
 import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { TypesRiskModel } from '../model/types-risk.model';
+import {FormType} from '../../../commons/form-type.enum';
+
 
 @Component({
-    selector     : 'types-dialog',
-    templateUrl  : './types-dialog.component.html',
-    styleUrls    : ['./types-dialog.component.scss'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'types-dialog',
+  templateUrl: './types-dialog.component.html',
+  styleUrls: ['./types-dialog.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class TypesDialogComponent implements OnInit
-{
-    showExtraToFields = false;
-    composeForm: FormGroup;
+export class TypesDialogComponent implements OnInit {
+  showExtraToFields = false;
+  composeForm: FormGroup;
+  typesRiskModel = new TypesRiskModel();
 
-    constructor(
-        public dialogRef: MatDialogRef<TypesDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) private data: any
-    )
-    {  }
+  constructor(
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<TypesDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: any
+  ) {
+  }
 
-    ngOnInit()
-    {
+  ngOnInit() {
+    this.composeForm = this.formBuilder.group({
+      tipo_riesgo: ['']
+    });
+    if ( this.data ) {
+      this.typesRiskModel = this.data.product;
+      this.dataForm();
     }
+  }
+
+  saveRisk() {
+    const tipeRisk = <TypesRiskModel> this.composeForm.getRawValue();
+    this.dialogRef.close(tipeRisk);
+  }
+
+  private dataForm() {
+    this.composeForm.setValue({
+      tipo_riesgo: this.typesRiskModel.tipo_riesgo
+    });
+  }
+
+  closeModal() {
+    this.dialogRef.close();
+  }
 
 
 }
--- src/app/main/content/system-tables/types/types.component.ts
+++ src/app/main/content/system-tables/types/types.component.ts
@@ -10,6 +10,7 @@ import { TypesRiskService } from '../../system-tables/types/service/types-risk.s
 })
 export class SystemTypesComponent implements OnInit {
     riskTypes: any[];
+    restData: any;
     dialogRef: any;
     loadingIndicator = true;
     reorderable = true;
@@ -19,14 +20,18 @@ export class SystemTypesComponent implements OnInit {
     }
 
     ngOnInit(){
+      this.riskTypes = [];
         this.typesRiskService.getTypeRisk().subscribe((data: any) => {
           this.riskTypes = data;
           this.loadingIndicator = false;
           });
     }
 
-    saveTypeRisk(){
-
+    saveTypeRisk(riskTypes){
+      this.typesRiskService.postTypeRisk(riskTypes[0]).subscribe((data: any) => {
+        this.restData = data;
+        console.log(this.restData);
+      });
     }
 
     typesDialog(){
@@ -35,7 +40,10 @@ export class SystemTypesComponent implements OnInit {
       });
       this.dialogRef.afterClosed()
         .subscribe(response => {
-
+          this.riskTypes.push(response);
+          this.saveTypeRisk(this.riskTypes);
+          this.riskTypes = [...this.riskTypes];
+          this.loadingIndicator = false;
         });
     }
 }
