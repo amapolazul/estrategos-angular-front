cb4bb33a4912977a2cf27dc99343aaaa26459f67
esteban <estebanbacl@outlook.com>
2018-07-21 09:43:25 -0500

    administracion del riesgo

--- src/app/main/content/riesgos/riesgos.module.ts
+++ src/app/main/content/riesgos/riesgos.module.ts
@@ -5,13 +5,19 @@ import { RiesgosComponent } from './riesgos.component';
 import {RiesgosListsComponent} from './tabs/listado/riesgos-lists.component';
 import {EjerciciosListsComponent} from './tabs/ejercicios/ejercicios-lists.component';
 import {EjercicioDialogComponent} from './tabs/ejercicios/dialog/ejercicio-dialog.component';
+import {AdministracionListsComponent} from './tabs/administracion/administracion-lists.component';
 
 
 const routes: Routes = [
     {
         path     : 'riesgos',
         component: RiesgosComponent
+    },
+    {
+      path     : 'administracion-riesgos',
+      component: AdministracionListsComponent
     }
+
 ];
 @NgModule({
     imports     : [
@@ -23,11 +29,13 @@ const routes: Routes = [
         RiesgosListsComponent,
         EjercicioDialogComponent,
         EjerciciosListsComponent,
+        AdministracionListsComponent
 
     ],
     entryComponents: [
         EjercicioDialogComponent,
-        EjerciciosListsComponent
+        EjerciciosListsComponent,
+        AdministracionListsComponent
     ]
 })
 export class RiesgosModule
