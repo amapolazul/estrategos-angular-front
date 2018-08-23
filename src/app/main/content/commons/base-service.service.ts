404ee43f01a62a233d59e608f916e74c17bba075
Juan Martinez <juanmartinez@embera.com.co>
2018-08-22 21:08:10 -0500

    Devuelve amazon

--- src/app/main/content/commons/base-service.service.ts
+++ src/app/main/content/commons/base-service.service.ts
@@ -3,9 +3,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export abstract class BaseService {
 
-  //private _generalUrl = 'http://52.4.220.100:9000/';
+  private _generalUrl = 'http://52.4.220.100:9000/';
   //private _generalUrl = 'http://localhost:9000/';
-  private _generalUrl = 'http://PGNSTRAAP03:9000/';
+  //private _generalUrl = 'http://PGNSTRAAP03:9000/';
 
   constructor() {
   }
