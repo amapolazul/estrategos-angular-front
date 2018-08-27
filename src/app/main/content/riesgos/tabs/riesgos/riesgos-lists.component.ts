cd135b63906a30dc0137407799b0e994a891310f
Juan Martinez <juanmartinez@embera.com.co>
2018-08-26 23:01:02 -0500

    Agrega funcionalidad de borrar riesgo

--- src/app/main/content/riesgos/tabs/riesgos/riesgos-lists.component.ts
+++ src/app/main/content/riesgos/tabs/riesgos/riesgos-lists.component.ts
@@ -3,6 +3,8 @@ import {RatingRiskService} from '../../../system-tables/rating/service/rating-ri
 import {CausesDialogComponent} from '../../../system-tables/causes/dialog/causes-dialog.component';
 import {FormType} from '../../../commons/form-type.enum';
 import {Router} from '@angular/router';
+import {RiesgosService} from '../../services/riesgos.service';
+import {CustomSnackBarMessages} from '../../../commons/messages.service';
 
 @Component({
     selector   : 'riesgos-lists',
@@ -14,9 +16,27 @@ export class RiesgosListsComponent
     rows = [];
 
     constructor(private calificacionRiesgoService: RatingRiskService,
+                private riesgosService: RiesgosService,
+                private snackBar: CustomSnackBarMessages,
                 private route: Router)
     {
 
+    }
+
+    delete(row, rowIndex) {
+      if (row.estatus_riesgo_id === 2) {
+        this.snackBar.openSnackBar('No es permitido borrar un riesgo en estado Mitigado');
+      } else {
+        this.riesgosService.borrarRiesgo(row.id).subscribe(x => {
+          if (rowIndex > -1) {
+            this.rows.splice(rowIndex, 1);
+            this.rows = [...this.rows];
+            this.snackBar.openSnackBar('Registro eliminado');
+          }
+        }, error => {
+          this.snackBar.openSnackBar('Ha ocurrido un error borrando el riesgo');
+        });
+      }
     }
 
     edit(row, rowIndex){
